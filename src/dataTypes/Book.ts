41316513146998fffaf028843169d707002e1d7a
Mathis Burger <handymathis@gmail.com>
2024-04-04 20:09:07 +0200

    [TASK] Added support for book

--- src/dataTypes/Book.ts
+++ src/dataTypes/Book.ts
@@ -1,31 +1,64 @@
 import {QuoteObject} from "../typings/QuoteObject";
 
 class Book implements QuoteObject {
-    readonly author: Author[];
+    readonly author: string[];
     readonly title: string;
     readonly releaseYear: string;
     readonly edition: string;
     readonly publisherLocation: string;
     readonly publisher: string;
+    readonly isEBook: boolean;
 
     constructor() {
         this.author = this.getAuthors();
         this.title = this.getTitle();
-        this.releaseYear = this.getTitleTableAttribute("Jahr");
+        this.releaseYear = this.getTitleTableAttribute("Jahr").replace("[", "").replace("]", "");
         this.edition = this.getTitleTableAttribute("Ausgabe");
         this.publisherLocation = this.getTitleTableAttribute("Verlagsort");
         this.publisher = this.getTitleTableAttribute("Verlag");
+        this.isEBook = this.getTitleTableAttribute("Erscheinungsform").indexOf("E-Book") > -1;
     }
 
     generateQuote(): string {
         if (this.edition === null) {
-            return `${this.author.join(', ')} (${this.releaseYear}) <i>${this.title}</i>, ${this.publisherLocation}, ${this.publisher}`;
+            return `${this.getAuthorString()} ${this.getHg()}(${this.releaseYear}) <i>${this.title}</i>${this.isEBook ? " [Online]" : ""}, ${this.publisherLocation}, ${this.publisher}. ${this.getAccessedAt()}`;
         } else {
-            return `${this.author.join(', ')} (${this.releaseYear}) <i>${this.title}</i>, ${this.edition}, ${this.publisherLocation}, ${this.publisher}`;
+            return `${this.getAuthorString()} ${this.getHg()}(${this.releaseYear}) <i>${this.title}</i>${this.isEBook ? " [Online]" : ""}, ${this.edition}, ${this.publisherLocation}, ${this.publisher}. ${this.getAccessedAt()}`;
         }
     }
 
-    private getAuthors(): Author[] {
+    private getAccessedAt(): string {
+        if (this.isEBook) {
+            const doiLink = this.getTitleTableAttribute("DOI");
+            const permaLink = this.getTitleTableAttribute("Permalink");
+            const link = doiLink ? `https://doi.org/${doiLink}` : permaLink
+            const date = new Date();
+            const formattedDate = `${date.getDate() < 10 ? "0" + date.getDate() : date.getDate()}.${date.getMonth() < 10 ? "0" + date.getMonth() : date.getMonth()}.${date.getFullYear()}`
+            return `Verfügbar unter ${link} (Abgerufen am ${formattedDate}).`
+        }
+        return "";
+    }
+
+    private getAuthorString(): string {
+        if (this.author.length === 1) {
+            return this.author[0];
+        } else if (this.author.length === 2) {
+            return this.author.join(' und ');
+        } else if (this.author.length === 3) {
+            return this.author[0] + ", " + this.author[1] + " und " + this.author[2];
+        } else if (this.author.length >= 4) {
+            return this.author[0] + " et al.";
+        }
+    }
+
+    private getHg(): string {
+        if (this.author[0].endsWith("[Herausgeber]")) {
+            return "(Hg.) ";
+        }
+        return "";
+    }
+
+    private getAuthors(): string[] {
         let elements = document.getElementsByClassName("author")[0].children;
         let arr = [];
         for (let i=0; i<elements.length; i++) {
